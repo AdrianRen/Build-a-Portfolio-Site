348a1e5b105cf5e9b3e6a0d59fa39327c501cfe3
Adrian Ren <adrian.ren@outlook.com>
2017-08-20 22:59:37 +1200

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,35 @@
-grunt.initConfig({
-  responsive_images: {
-    dev: {
-      options: {},
-      sizes: [{
-        width: 320,
-        height: 240
-      },{
-        name: 'large',
-        width: 640
-      },{
-        name: "large",
-        width: 1024,
-        suffix: "_x2",
-        quality: 30
-      }],
-      files: [{
-        expand: true,
-        src: ['*.{jpg,gif,png}'],
-        cwd: 'images/',
-        dest: 'resized_images/'
-      }]
-    }
-  }
-});
-grunt.loadNpmTasks('grunt-responsive-images');
-grunt.registerTask('default', ['responsive_images']);
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {},
+        sizes: [{
+          name: 'small',
+          width: 320,
+          height: 240
+        }, {
+          name: 'large',
+          width: 640
+        }, {
+          name: "large",
+          width: 1024,
+          suffix: "_x2",
+          quality: 60
+        }],
+        files: [{
+          expand: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+
+  grunt.registerTask('default', ['responsive_images']);
+};
